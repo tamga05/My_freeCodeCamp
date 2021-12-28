ec7a2e8bc33f3f4ae94185a458605fa5001d5e9c
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-28 11:46:55 +0300

    tasks solving

---
+++ tasks/word-blanks.js
@@ -0,0 +1,12 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+
+const wordBlanks = "My " + myNoun + ' very ' + myAdjective + ' in this moment ' + myVerb + ' very ' + myAdverb + '.'; // Change this line
+
+// Only change code above this line
+
+console.log(wordBlanks);
