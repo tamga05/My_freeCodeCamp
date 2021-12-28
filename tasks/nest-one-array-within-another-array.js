c7d42753a0bc27710a9723c1d0611793c045c404
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-28 12:05:01 +0300

    tasks solving

---
+++ tasks/nest-one-array-within-another-array.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myArray = [[0, 1, '2'], [3, 'number', 'pi'], ['zero', 4]];
+console.log(myArray);
---
+++ tasks/store-multiple-values-in-one-variable-using-javascript-arrays.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myArray = ['number', 3.14];
+console.log(myArray);
