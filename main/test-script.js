e6cda5e54a146c1e5482a370278737cba15e539b
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-29 09:25:38 +0300

    tasks solving

--- main/test-script.js
+++ main/test-script.js
@@ -1,3 +1,34 @@
-const myName = "Andrey Nikitin";
-const myStr = "My name is " + myName + " and I am well!";
-console.log(myStr);
+// let answer = confirm("Бро,ты хорошо подумал, что делаешь???");
+// console.log(answer);
+
+// let answer = prompt("Ваше имя?", "");
+// console.log('Ваше имя: ' + answer);
+
+// let age = prompt('Ваш возраст?', '');
+// alert(age);
+
+// let num1 = prompt('Введите первое число', '');
+// let num2 = prompt('Введите второе число', '');
+
+// console.log(num1 + num2); // сложит числа как строки
+// console.log(Number(num1) + Number(num2)); // сложит числа как числа
+
+// let s = prompt("Назовите сторону квадрата", "");
+// console.log(Number(s)*Number(s));
+
+// let a = prompt("Назовите одну сторону прямоугольника", "");
+// let b = prompt("Назовите другую сторону прямоугольника", "");
+// console.log((Number(a) + Number(b))*2);
+
+// let a = +prompt("Назовите одну сторону прямоугольника", "");
+// let b = +prompt("Назовите другую сторону прямоугольника", "");
+// console.log((a + b) * 2);
+
+let arr = [];
+
+arr[0] = prompt('Как тебя зовут?');
+arr[1] = prompt('Ты откуда');
+arr[2] = prompt('Сколько тебе лет');
+
+console.log(arr);
+console.log(typeof(arr));
