0521b967446d117bcc7314720fd285cb1214023e
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-01-16 13:59:35 +0300

    tasks solving

---
+++ tasks/js-algorithms-and-data-structures/accessing-object-properties-with-bracket-notation.js
@@ -0,0 +1,14 @@
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+testObj["an entree"];
+testObj["the drink"];
+
+const entreeValue = "hamburger";   // Change this line
+const drinkValue = "water";    // Change this line
