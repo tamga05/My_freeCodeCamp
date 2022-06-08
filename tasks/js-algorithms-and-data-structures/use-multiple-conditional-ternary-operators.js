723544c3da56337787a898f019e8b3164dbdd2f9
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2022-06-08 16:49:15 +0300

    task solving

---
+++ tasks/js-algorithms-and-data-structures/use-multiple-conditional-ternary-operators.js
@@ -0,0 +1,13 @@
+function checkSign(num) {
+  return (num > 0) ? "positive"
+   : (num < 0) ? "negative"
+   : "zero";
+}
+
+// function checkSign(num) {
+// return num > 0 ? "positive"
+// : num < 0 ? "negative"
+// : "zero";
+// }
+
+checkSign(10);
